79784e87ab1013663aebf5795c0089602bbfba6e
Sean Lopp <lopp@elementl.com>
2023-02-08 20:05:14 -0700

    try canary again

--- setup.py
+++ setup.py
@@ -6,15 +6,6 @@ if __name__ == "__main__":
         packages=find_packages(),
         package_data={"dagster_project": ["dbt_project/*"]},
         install_requires=[
-            "dagster",
-            "dagster-cloud",
-            "dagster-dbt",
-            "pandas",
-            "numpy",
-            "scipy",
-            "dbt-core",
-            "dbt-duckdb",
-            "requests",
         ],
         extras_require={"dev": ["dagit", "pytest"]},
     )
